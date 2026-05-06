f5a45dab15995022e0d45257731bffff9bfd8184
Leonardo Quinones <leonardo.quinones@unity3d.com>
2026-05-06 01:43:26 -0500

    bat script updated

--- build_and_run.bat
+++ build_and_run.bat
@@ -20,6 +20,15 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
+REM Copy DLLs to bin directory if they don't exist
+if not exist bin\SDL2.dll (
+    echo Copying SDL2 DLLs to bin directory...
+    copy SDL2-2.30.5\x86_64-w64-mingw32\bin\SDL2.dll bin\ >nul 2>&1
+    copy SDL2_image-2.8.2\x86_64-w64-mingw32\bin\*.dll bin\ >nul 2>&1
+    copy SDL2_ttf-2.22.0\x86_64-w64-mingw32\bin\*.dll bin\ >nul 2>&1
+    copy SDL2_mixer-2.8.0\x86_64-w64-mingw32\bin\*.dll bin\ >nul 2>&1
+)
+
 echo Running TwoDEngine...
 bin\main.exe
 
